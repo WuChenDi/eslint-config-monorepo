e9cec5c1b8ab20d89c47a34541a9396b97b15df1
wudi <wuchendi96@gmail.com>
2023-08-04 16:26:49 +0800

    feat: add next examples

--- packages/eslint-config-next/index.js
+++ packages/eslint-config-next/index.js
@@ -8,7 +8,23 @@ module.exports = {
     React: true,
     JSX: true,
   },
-  extends: ['@cdlab996/eslint-config-react', 'next', 'plugin:mdx/recommended'],
+  extends: [
+    '@cdlab996/eslint-config-react',
+    /**
+     * TODO: Pending error
+     *
+     * To run "pnpm run lint" in the next-demo project without errors, disable the plugin:react-hooks/recommended in eslint-config-react.
+     *
+     * Plugin "react-hooks" was conflicted between ".eslintrc.json » @cdlab996/eslint-config » @cdlab996/eslint-config-next » eslint-config-next » plugin:react-hooks/recommended" and ".eslintrc.json » @cdlab996/eslint-config » @cdlab996/eslint-config-next » @cdlab996/eslint-config-react » plugin:react-hooks/recommended".
+     */
+    'next',
+    /**
+     * whether it is established
+     *
+     * 'next' !== 'eslint-config-next',
+     */
+    'plugin:mdx/recommended',
+  ],
   settings: {
     react: {
       version: 'detect',
